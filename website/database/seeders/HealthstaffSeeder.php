415f39cf4f39ae267208a7ee9f6c0b2280e60fbe
GeorgiusKurli <georgius.e.k@gmail.com>
2021-05-08 15:14:33 +0700

    added healthstaff role

---
+++ website/database/seeders/HealthstaffSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class HealthstaffSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'health',
+            'email' => 'test@test.com',
+            'password' => Hash::make('password'),
+            'first_name' => 'health',
+            'last_name' => 'staff',
+            'vaccinated' => true,
+        ]);
+
+        DB::table('model_has_roles')->insert([
+            'role_id' => 2,
+            'model_type' => 'App\Models\User',
+            'model_id' => 2,
+        ]);
+
+        DB::table('health_staffs')->insert([
+            'user_id' => 2,
+            'position' => "General Practitioner",
+            'businesses_id' => 1,
+            'health_org_email' => "test@test.com",
+            'verified' => true,
+        ]);
+    }
+}
