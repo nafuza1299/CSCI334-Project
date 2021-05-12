565d5e515e3b3df4263d0d161059dca293dc3d77
GeorgiusKurli <georgius.e.k@gmail.com>
2021-05-12 14:34:19 +0700

    Fix covidtests table migration

--- website/database/migrations/2021_05_12_072600_create_covidtests_table.php
+++ website/database/migrations/2021_05_12_072600_create_covidtests_table.php
@@ -17,7 +17,7 @@ class CreateCovidtestsTable extends Migration
             $table->id();
             $table->timestamps();
             $table->foreignId('user_id')->constrained();
-            $table->timestamp("test_date");
+            $table->date("test_date");
             $table->string("location");
         });
     }
