597c708fe061c5ae3a2f28973d33bbb593f19c8f
unknown <nafuza1299@gmail.com>
2021-05-16 16:58:11 +0700

    covid19 location

---
+++ website/resources/views/location.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.layouts')
+@section('content')
+<style>
+.row-status {
+    display : flex;
+    align-items : center;
+}
+.box {
+  height: 20px;
+  width: 40px;
+  margin-right : 5px;
+}
+.green {
+  background-color: green;
+}
+.red {
+  background-color: red;
+}
+.yellow {
+  background-color: yellow;
+}
+</style>
+<section>
+    <body>
+        <div class="container block-7 mb-5">	
+			<div class="slider-text align-items-center" style="height:inherit !important">
+				<div class="col-md-12 d-flex align-items-end">
+					<div class="text">
+						<h1  style="font-size:40px !important">Location by COVID-19 infection</h1>
+					</div>
+				</div>
+                <div class="col-md-12 d-flex align-items-end">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Status</th>
+                                <th scope="col">Location</th>
+                                <th scope="col">Number of cases</th>
+                                <th scope="col">Latitude</th>
+                                <th scope="col">Longitude</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($result as $data)
+                                <tr>
+                                    <th scope="row">
+                                        <div class="row-status">
+                                        @if($data->total < 5)
+                                            <div class='box green'></div>
+                                            <span>Safe</span>
+                                        @elseif($data->total >= 5)
+                                            <div class='box yellow'></div>
+                                            <span>Mild</span>
+                                        @elseif($data->total >= 10)
+                                            <div class='box red'></div>
+                                            <span>Severe</span>
+                                        @endif
+                                        </div>
+                                    </th>
+                                    <td>{{ $data->address }}</td>
+                                    <td>{{ $data->total }}</td>
+                                    <td>{{ $data->latitude }}</td>
+                                    <td>{{ $data->longitude }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+			</div>
+		</div>
+       
+    </body>
+</section>
+@endsection
