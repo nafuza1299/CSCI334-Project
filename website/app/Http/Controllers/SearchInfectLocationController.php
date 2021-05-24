9d2b3793e29f8d308bb8d8b2ab4af99312b637fc
unknown <nafuza1299@gmail.com>
2021-05-24 16:25:01 +0700

    infection singleton

--- website/app/Http/Controllers/SearchInfectLocationController.php
+++ website/app/Http/Controllers/SearchInfectLocationController.php
@@ -2,23 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\CheckIn;
-use App\Models\TestResult;
-
 class SearchInfectLocationController extends Controller
 {
     public function index()
     {
         //get users which are infected
-        $testresult = new TestResult;
-        $test_result_data = $testresult->getInfected();
+        $test_result_data = app("TestResult")->getInfected();
       
         //store id as an array after mapping
         $getUserID = array_filter(array_map(function($data) { return $data['user_id']; }, $test_result_data));
         
         //get areas where infected users have visited
-        $checkin = new CheckIn;
-        $result = $checkin->getPositiveVisitedAddress($getUserID);
+        $result = app("CheckIn")->getPositiveVisitedAddress($getUserID);
+
         return view('location', compact('result'));
     }
 }
