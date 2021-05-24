748b4e2bd4644a3ef3083839c1d814ad39fb1a64
unknown <nafuza1299@gmail.com>
2021-05-24 15:25:20 +0700

    check in singleton

--- website/app/Http/Controllers/CheckInController.php
+++ website/app/Http/Controllers/CheckInController.php
@@ -2,15 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\CheckIn;
-
 class CheckInController extends Controller
 {
     public function index()
     {
         //retrieves check-in data of user
-        $checkin= new CheckIn();
-        $checkin_data = $checkin->getCheckIn(auth()->id());
+        $checkin_data = app("CheckIn")->getCheckIn(auth()->id());
    
         return view('user.history', compact('checkin_data'));
     }
