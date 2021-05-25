0e7ccaa2ed5eda578474049d398e3f0e5c5ef5d0
unknown <nafuza1299@gmail.com>
2021-05-26 00:13:41 +0700

    generate qr code fix

--- website/resources/views/organization/generate-qr-code.blade.php
+++ website/resources/views/organization/generate-qr-code.blade.php
@@ -11,33 +11,51 @@
                 <div>
             </div>
         @else
-        <div class="container block-7" style="background-color:#FFF; border: 1px solid black !important; max-width: 600px !important">
-            <h3>Generate a <b>QR Code</b> for your business location</h3>
-            <!-- Validation Errors -->
-            <x-auth-validation-errors class="mb-4" :errors="$errors" style="color:red;" />
-            <form method="POST" action="{{ route('generate.qr.code') }}">
-                @csrf
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group">
-                            <select class="form-control form-control-lg empty select-address" name="id" required>
-                                <option value="" selected disabled hidden>Select Business Address</option>
-                                @foreach ($address as $data)
-                                    <option value="{{$data->id}}">{{$data->address}}</option>
-                                @endforeach
-                            </select>
-                            <!-- <x-input id="username" class="form-control" type="text" name="username" :value="old('username')" required placeholder="Username" autofocus/> -->
-                        </div>
+        <div class="container mb-5">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="slider-text align-items-center" style="height:inherit !important">
+						<div class="text">
+							<h1  style="font-size:40px !important">Safe Registration</h1>
+							<h2>Hi, {{{Auth::guard('business')->user()->username}}}</h2>
+						</div>
+					</div>
+				</div>
+                <div class="col-md-12">
+                    <div class="container block-7" style="background-color:#FFF; border: 1px solid black !important; max-width: 600px !important">
+                        <h3>Generate a <b>QR Code</b> for your business location</h3>
+                        <!-- Validation Errors -->
+                        <x-auth-validation-errors class="mb-4" :errors="$errors" style="color:red;" />
+                        <form method="POST" action="{{ route('generate.qr.code') }}">
+                            @csrf
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <select class="form-control form-control-lg empty select-address" name="id" required>
+                                            <option value="" selected disabled hidden>Select Business Address</option>
+                                            @if(count($address) > 0)
+                                                @foreach ($address as $data)
+                                                    <option value="{{$data->id}}">{{$data->address}}</option>
+                                                @endforeach
+                                            @else
+                                                <option value="redirect" href="{{route('business.profile')}}">Register Business Address</option>
+                                            @endif
+                                        </select>
+                                        <!-- <x-input id="username" class="form-control" type="text" name="username" :value="old('username')" required placeholder="Username" autofocus/> -->
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <x-button class="btn btn-primary py-3 px-4">
+                                            {{ __('Generate QR Code') }}
+                                        </x-button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                    <div class="col-md-12">
-                        <div class="form-group">
-                            <x-button class="btn btn-primary py-3 px-4">
-                                {{ __('Generate QR Code') }}
-                            </x-button>
-                        </div>
-                    </div>
-                </div>
-            </form>
+                </div>  
+            </div>  
         </div>
         @endif
     </body>
@@ -46,8 +64,10 @@
 @section('javascript')
 <script>
     $('.select-address').on('change', function() {
-        // console.log( this.value );
-        });
+        if(this.value == "redirect" ){
+            window.location = "{{route('business.profile')}}" // redirect
+        }
+    });
 </script>
 @if(session()->has('qrcode'))
     <script type="text/javascript">
