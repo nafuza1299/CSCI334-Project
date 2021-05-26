e609dc544871941df7454cb639bcc120bbdb022f
unknown <nafuza1299@gmail.com>
2021-05-26 18:21:37 +0700

    vaccine and safe regis fix

--- website/app/Http/Controllers/FileDownloadControllers/UserFileDownloadController.php
+++ website/app/Http/Controllers/FileDownloadControllers/UserFileDownloadController.php
@@ -9,10 +9,19 @@ class UserFileDownloadController extends FileDownloadController
 {
     public function download_certs($file_name)
     {
+        return Storage::download('user/certs/'.$file_name);
+    }
+    public function self_download_certs()
+    {
+        $user = auth()->user()->certificate;
+        return $this->download_certs($user);
+        
+    }
+    public function admin_download_certs($file_name)
+    {
         $user = auth()->user();
         if($user->hasPermissionTo('update users health status')){
-            return Storage::download('user/certs/'.$file_name);
+            return $this->download_certs($file_name);
         }
-        
     }
 }
